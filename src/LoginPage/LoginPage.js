ee5398afa00cdb3fa35a274c0c64e6cc0bb9c912
xorua117 <xorua117@gmail.com>
2021-06-19 19:34:56 +0900

    create login page

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import logo from '../resources/logo.png';
+import UsernameInput from './components/UsernameInput';
+import SubmitButton from './components/SubmitButton';
+import { useHistory } from 'react-router';
+import { setUsername } from '../store/actions/dashboardActions';
 
-const LoginPage = () => {
+import './LoginPage.css';
+
+const LoginPage = ({ saveUsername}) => {
+  const [username, setUsername] = useState('');
+
+  const history = useHistory();
+
+  const handleSubmitButtonPressed = () => {
+    history.push('/dashboard');
+    saveUsername(username);
+  };
+
   return (
-    <div>
-      Login Page
+    <div className='login-page_container background_main_color'>
+      <div className='login-page_login_box background_secondary_color'>
+        <div className='login-page_logo_container'>
+          <img className='login-page_logo_image' src={logo} alt="VideoTalker" />
+        </div>
+        <div className='login-page_title_container'>
+          <h2>Get on Board</h2>
+        </div>
+        <UsernameInput username={username} setUsername={setUsername} />
+        <SubmitButton handleSubmitButtonPressed={handleSubmitButtonPressed} />
+      </div>
     </div>
   );
 };
 
-export default LoginPage;
+const mapActionsToProps = (dispatch) => {
+  return {
+    saveUsername: username => dispatch(setUsername(username))
+  };
+};
+
+export default connect(null, mapActionsToProps)(LoginPage);
