81391bcb06c72125f2d0434957d23d68be972600
xorua117 <xorua117@gmail.com>
2021-06-22 00:51:35 +0900

    Removing users from the server at disconnection - frontend

--- src/components/ActiveUsersList/ActiveUsersList.js
+++ src/components/ActiveUsersList/ActiveUsersList.js
@@ -4,25 +4,6 @@ import { connect } from 'react-redux';
 
 import './ActiveUsersList.css';
 
-// const activeUsers = [
-//   {
-//     socketId: 321,
-//     username: 'Paul'
-//   },
-//   {
-//     socketId: 333,
-//     username: 'John'
-//   },
-//   {
-//     socketId: 432,
-//     username: 'Kate'
-//   },
-//   {
-//     socketId: 345,
-//     username: 'Adam'
-//   }
-// ];
-
 const ActiveUsersList = ({ activeUsers }) => {
   return (
     <div className='active_user_list_container'>
