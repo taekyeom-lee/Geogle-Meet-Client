f6e8fd406c4be9e04d860b7ac74aae6f650c1359
xorua117 <xorua117@gmail.com>
2021-06-22 01:10:55 +0900

    Getting local stream - frontend

---
+++ src/store/reducers/callReducer.js
@@ -0,0 +1,19 @@
+import * as callActions from '../actions/callActions';
+
+const initState = {
+  localStream: null
+};
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case callActions.CALL_SET_LOCAL_STREAM:
+      return {
+        ...state,
+        localStream: action.localStream
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
