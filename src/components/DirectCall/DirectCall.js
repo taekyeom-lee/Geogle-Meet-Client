cd26343b5696456336300b47edaa5fea9b66a145
xorua117 <xorua117@gmail.com>
2021-06-26 23:03:14 +0900

    Creating conversation buttons

--- src/components/DirectCall/DirectCall.js
+++ src/components/DirectCall/DirectCall.js
@@ -6,6 +6,7 @@ import CallRejectedDialog from '../CallRejectedDialog/CallRejectedDialog';
 import IncomingCallDialog from '../IncomingCallDialog/IncomingCallDialog';
 import CallingDialog from '../CallingDialog/CallingDialog';
 import { callStates, setCallRejected } from '../../store/actions/callActions';
+import ConversationButtons from '../ConversationButtons/ConversationButtons';
 
 const DirectCall = (props) => {
   const {
@@ -28,6 +29,7 @@ const DirectCall = (props) => {
       />}
       {callState === callStates.CALL_REQUESTED && <IncomingCallDialog callerUsername={callerUsername} />}
       {callingDialogVisible && <CallingDialog />}
+      <ConversationButtons />
     </>
   );
 };
--- src/store/reducers/callReducer.js
+++ src/store/reducers/callReducer.js
@@ -8,7 +8,8 @@ const initState = {
   callRejected: {
     rejected: false,
     reason: ''
-  }
+  },
+  remoteStream: null
 };
 
 const reducer = (state = initState, action) => {
@@ -38,9 +39,14 @@ const reducer = (state = initState, action) => {
         ...state,
         callRejected: action.callRejected
       };
+    case callActions.CALL_SET_REMOTE_STREAM:
+      return {
+        ...state,
+        remoteStream: action.remoteStream
+      }
     default:
       return state;
   }
-}
+};
 
 export default reducer;
